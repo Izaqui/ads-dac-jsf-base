22d9d1b096324a8e73a02e4c674b186563faa095
Gustvo98 <gustavocontas@gmail.com>
2022-08-20 13:15:22 -0300

    adicionando buscar editoras

--- postgres/insert.sql
+++ postgres/insert.sql
@@ -3,3 +3,5 @@ INSERT INTO livros(titulo, dataDeLancamento) VALUES ('Java em 21 dias','2011-02-
 -- INSERT INTO livros(titulo, dataDeLancamento) VALUES ('Descomplicando o Docker','2012-02-03');
 -- INSERT INTO livros(titulo, dataDeLancamento) VALUES ('Como programa em C++','2000-09-09');
 INSERT INTO editoras(localDeOrigem, nomeFantasia) VALUES ('Cajazeiras', 'Marvel CZ' );
+INSERT INTO editoras(localDeOrigem, nomeFantasia) VALUES ('Sousa', 'Alado' );
+INSERT INTO editoras(localDeOrigem, nomeFantasia) VALUES ('Jota', 'MPA' );
